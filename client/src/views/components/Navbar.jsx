5153a6d0367ea3ec52c61e55748e1052ed013406
Balev,Kristiyan K.E <k.balev@student.fontys.nl>
2022-05-20 11:53:42 +0200

    first navbar added

---
+++ client/src/views/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <a href='#'>Compiler</a>
+            <a href='#'>Access Manager</a>
+        </nav>
+    );
+}
+
+export default Navbar
