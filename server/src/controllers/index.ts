0cd66025744b4fb1b550209165ef9a8fe42a88f8
kalopresli <82675438+kalopresli@users.noreply.github.com>
2022-05-20 11:56:08 +0200

    test1

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import testRoutes from './TestController';
+import testRoutes1 from './test1';
 const controllerRoutes = Router();
 
 controllerRoutes.use(testRoutes);
+controllerRoutes.use(testRoutes1);
 
 export default controllerRoutes;
