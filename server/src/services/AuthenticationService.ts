5b89dbfced0882e6ae22ccc92ffe52d27325cf9e
RikThePixel <63408969+Rikthepixel@users.noreply.github.com>
2022-05-24 11:58:45 +0200

    Make mock implementation of GetAllUsers

--- server/src/services/AuthenticationService.ts
+++ server/src/services/AuthenticationService.ts
@@ -1,5 +1,21 @@
-export const GetAllUsers = async () => {
-    return [];
+import { IUser } from '../models/IUserModel';
+
+const getUserTable = async (): Promise<any> => {
+    return {
+        "user": "password"
+    };
+};
+
+export const GetAllUsers = async (): Promise<Array<IUser>> => {
+    const usersTable = await getUserTable();
+    const usersArray = Object.keys(usersTable).map(
+        (key) => ({
+            username: key,
+            password: usersTable[key]
+        })
+    );
+
+    return usersArray;
 };
 
 export const RegisterUser = async (username: String, password: String) => {
