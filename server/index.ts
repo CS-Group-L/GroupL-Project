691737e2ecc2c586eae975d5d3acee094735c0f2
RikThePixel <63408969+Rikthepixel@users.noreply.github.com>
2022-05-25 10:38:38 +0200

    Add body-parser and file upload-parser

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,19 @@
 import Express from 'express';
+import fileUpload from "express-fileupload";
 import controllerRoutes from './src/controllers';
 import { config } from 'dotenv';
+import bodyParser from 'body-parser';
+import { Execute } from './src/services/ClusterService';
 config();
 
 const port = process.env.PORT ?? 3000;
 const app = Express();
 
+app.use(fileUpload({ useTempFiles: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(controllerRoutes);
+Execute();
 
 app.listen(port, () => {
     console.log(`Server started on: http://localhost:${port}`);
