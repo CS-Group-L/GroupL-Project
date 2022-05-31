1ccd50c8135ad4b1aceb48ef46a19493231e98a1
RikThePixel <63408969+Rikthepixel@users.noreply.github.com>
2022-05-31 12:41:36 +0200

    implement websockets

--- server/src/controllers/ClusterController.ts
+++ server/src/controllers/ClusterController.ts
@@ -2,40 +2,50 @@ import { Router, Request, Response } from "express";
 import { UploadedFile } from 'express-fileupload';
 import { open } from 'fs/promises';
 import path from 'path';
-import { Execute, GetConsoleLog, PushToCluster } from '../services/ClusterService';
-const ClusterController = Router();
+import { Socket, Server as WsServer } from 'socket.io';
+import { Execute, PushToCluster, ConsoleOutput, ConsoleOutputLog } from '../services/ClusterService';
 
-ClusterController.post("/push", async (req: Request, res: Response) => {
-    if (!req?.files?.file) return res.sendStatus(400);
+export default (io: WsServer) => {
+    const ClusterController = Router();
+    const ClusterIO = io.of("Cluster");
 
-    const file = req.files.file as UploadedFile;
-    if (path.extname(file.name) !== ".py") return res.sendStatus(400);
+    ClusterController.post("/push", async (req: Request, res: Response) => {
+        if (!req?.files?.file) return res.sendStatus(400);
 
-    const uploaded = await PushToCluster(file.tempFilePath);
+        const file = req.files.file as UploadedFile;
+        if (path.extname(file.name) !== ".py") return res.sendStatus(400);
 
-    if (!uploaded) return res.sendStatus(500);
-    Execute();
-    return res.sendStatus(200);
-});
+        const uploaded = await PushToCluster(file.tempFilePath);
 
-ClusterController.post("/pushcode", async (req: Request, res: Response) => {
-    if (!req.body.code) return res.sendStatus(400);
-    const tempFilePath = `./${Math.floor(Math.random() * 100000000)}.py`;
+        if (!uploaded) return res.sendStatus(500);
+        Execute();
+        return res.sendStatus(200);
+    });
 
-    const file = await open(tempFilePath, "w");
-    await file.writeFile(req.body.code);
-    await file.close();
-    const uploaded = await PushToCluster(tempFilePath);
+    ClusterController.post("/pushcode", async (req: Request, res: Response) => {
+        if (!req.body.code) return res.sendStatus(400);
+        const tempFilePath = `./${Math.floor(Math.random() * 100000000)}.py`;
 
-    if (!uploaded) return res.sendStatus(500);
-    Execute();
-    return res.sendStatus(200);
-});
+        const file = await open(tempFilePath, "w");
+        await file.writeFile(req.body.code);
+        await file.close();
+        const uploaded = await PushToCluster(tempFilePath);
 
-ClusterController.get("/output", async (req: Request, res: Response) => {
-    return res.send(
-        JSON.stringify(await GetConsoleLog())
-    );
-});
+        if (!uploaded) return res.sendStatus(500);
+        Execute();
+        return res.sendStatus(200);
+    });
 
-export default ClusterController;
+    ClusterIO.on("connection", (socket: Socket) => {
+        const OutputReceptionHandler = (output: string) => socket.emit("output", output);
+        ConsoleOutput.on("data", OutputReceptionHandler);
+
+        socket.on("all-output", () => ConsoleOutputLog);
+
+        socket.on("disconnect", () => {
+            ConsoleOutput.off("data", OutputReceptionHandler);
+        });
+    });
+
+    return ClusterController;
+};
