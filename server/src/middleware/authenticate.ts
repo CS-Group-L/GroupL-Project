84fb812ee8c3156f699e0de5e9c22231815e2f1a
RikThePixel <63408969+Rikthepixel@users.noreply.github.com>
2022-06-14 10:09:00 +0200

    fix authenticate middleware

--- server/src/middleware/authenticate.ts
+++ server/src/middleware/authenticate.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 import { Socket } from 'socket.io';
-import { verifyAccessToken } from '../services/AuthenticationService';
+import { AuthService } from '../services/AuthenticationService';
+const service = new AuthService({
+    storagePath: "./data/users",
+    accessTokenSecret: process.env.ACCESS_TOKEN_SECRET
+});
 
 interface IAuthenticationData {
     [key: string]: string;
@@ -16,7 +20,7 @@ export const authenticate = async (req: Request, res: Response, next: NextFuncti
     const token = authHeader && authHeader.split(' ')[1];
     if (!token) return res.sendStatus(401);
     try {
-        const data = await verifyAccessToken(token);
+        const data = await service.verifyAccessToken(token);
         req.auth = data;
         next();
     }
